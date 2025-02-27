bb07c975d8eb4e5bbf9328a2a0f1d46892753e0b
Addon Bone <jacobneemila@gmail.com>
2025-02-28 00:52:28 +0200

    Create validation for background and command options

--- src/cli/resolvers/entrypoint/option.ts
+++ src/cli/resolvers/entrypoint/option.ts
@@ -1,36 +1,59 @@
+import {z} from "zod";
+
 import {OptionFile} from "../../parsers/entrypoint";
 
 import {BackgroundEntrypointOptions} from "@typing/background";
-import {EntrypointOptions} from "@typing/entrypoint";
 import {CommandEntrypointOptions} from "@typing/command";
+import {Browser} from "@typing/browser";
 
-const commonProperties: Array<keyof EntrypointOptions> = [
-    'includeApp',
-    'excludeApp',
-    'includeBrowser',
-    'excludeBrowser',
-];
+const CommonPropertiesSchema = z.object({
+    includeApp: z.array(z.string()).optional(),
+    excludeApp: z.array(z.string()).optional(),
+    includeBrowser: z.array(z.nativeEnum(Browser)).optional(),
+    excludeBrowser: z.array(z.nativeEnum(Browser)).optional(),
+});
+
+const parseOptions = <T extends typeof CommonPropertiesSchema>(file: string, schema: T, definition: string) => {
+    const options = OptionFile.make(file)
+        .setProperties(Object.keys(schema.shape))
+        .setDefinition(definition)
+        .getOptions();
+
+    const {success, error, data} = schema.safeParse(options);
+
+    if (!success) {
+        const e = error?.errors[0];
+
+        throw new Error(`Invalid options ${e.path.join(', ')} in "${file}": ${e.message}`);
+    }
+
+    return data;
+}
 
 export const getBackgroundOptions = (file: string): BackgroundEntrypointOptions => {
-    return OptionFile.make(file)
-        .setDefinition('defineBackground')
-        .setProperties([...commonProperties, 'persistent'])
-        .getOptions();
+    const BackgroundPropertiesSchema = CommonPropertiesSchema.extend({
+        persistent: z.boolean().optional(),
+    });
+
+    return parseOptions(file, BackgroundPropertiesSchema, 'defineBackground');
 }
 
 export const getCommandOptions = (file: string): CommandEntrypointOptions => {
-    return OptionFile.make(file)
-        .setDefinition('defineCommand')
-        .setProperties([
-            ...commonProperties,
-            'name',
-            'description',
-            'global',
-            'defaultKey',
-            'windowsKey',
-            'macKey',
-            'chromeosKey',
-            'linuxKey'
-        ])
-        .getOptions();
+    const ShortcutKeySchema = z
+        .string()
+        .regex(/^(Ctrl|Command|MacCtrl|Alt|Option)(\+Shift)?\+[A-Z0-9]$/, 'Invalid shortcut key, expected format like: Ctrl+Shift+K or Command+Shift+P')
+        .optional();
+
+    const CommandPropertiesSchema = CommonPropertiesSchema.extend({
+        name: z.string().nonempty().optional(),
+        description: z.string().nonempty().optional(),
+        global: z.boolean().optional(),
+        defaultKey: ShortcutKeySchema,
+        windowsKey: ShortcutKeySchema,
+        macKey: ShortcutKeySchema,
+        chromeosKey: ShortcutKeySchema,
+        linuxKey: ShortcutKeySchema,
+    });
+
+    return parseOptions(file, CommandPropertiesSchema, 'defineCommand');
 }

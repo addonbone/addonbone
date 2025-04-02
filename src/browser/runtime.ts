bb40ff11cc3a85af8432a9460bd05b64456d4824
Rostyslav Nihrutsa <rostyslav.nihrutsa@gmail.com>
2025-04-02 19:57:38 +0300

    Updated runtime api MBA-77

--- src/browser/runtime.ts
+++ src/browser/runtime.ts
@@ -2,6 +2,9 @@ import {browser} from "./env";
 import {ManifestVersion} from "@typing/manifest";
 
 type Manifest = chrome.runtime.Manifest;
+type PlatformInfo = chrome.runtime.PlatformInfo;
+type ContextFilter = chrome.runtime.ContextFilter;
+type ExtensionContext = chrome.runtime.ExtensionContext;
 
 const runtime = browser().runtime;
 
@@ -9,29 +12,53 @@ const backgroundPaths = [
     '/_generated_background_page.html',
 ];
 
-export const getUrl = (path: string) => runtime.getURL(path);
+export const getRuntimeId = (): string => runtime.id;
 
-export const getManifest = (): Manifest => runtime.getManifest();
+export const getRuntimeUrl = (path: string) => runtime.getURL(path);
 
-export const getManifestVersion = (): ManifestVersion => getManifest().manifest_version;
+export const getRuntimeManifest = (): Manifest => runtime.getManifest();
 
-export const isManifestVersion3 = (): boolean => getManifestVersion() === 3;
+export const getRuntimeManifestVersion = (): ManifestVersion => getRuntimeManifest().manifest_version;
 
-export const getId = (): string => runtime.id;
+export const getRuntimeContexts = (filter: ContextFilter) => new Promise<ExtensionContext[]>((resolve, reject) => {
+    runtime.getContexts(filter, contexts => {
+        try {
+            throwRuntimeError();
+
+            resolve(contexts);
+        } catch (e) {
+            reject(e);
+        }
+    });
+});
+
+export const getRuntimePlatformInfo = (): Promise<PlatformInfo> => new Promise<PlatformInfo>((resolve, reject) => {
+    runtime.getPlatformInfo((platformInfo) => {
+        try {
+            throwRuntimeError();
+
+            resolve(platformInfo);
+        } catch (e) {
+            reject(e);
+        }
+    });
+});
+
+export const isManifestVersion3 = (): boolean => getRuntimeManifestVersion() === 3;
 
 export const isBackground = (): boolean => {
-    if (!getId()) {
+    if (!getRuntimeId()) {
         return false;
     }
 
-    const manifest = getManifest();
+    const manifest = getRuntimeManifest();
 
     if (!manifest.background) {
         return false;
     }
 
     if (manifest.manifest_version === 3) {
-        return window === undefined;
+        return typeof window === "undefined";
     }
 
     return window !== undefined && backgroundPaths.includes(location.pathname);
@@ -43,4 +70,10 @@ export const throwRuntimeError = (): void => {
     if (error) {
         throw new Error(error.message);
     }
+}
+
+export const onRuntimeInstalled = (callback: Parameters<typeof chrome.runtime.onInstalled.addListener>[0]) => {
+    runtime.onInstalled.addListener(callback);
+
+    return () => runtime.onInstalled.removeListener(callback);
 }

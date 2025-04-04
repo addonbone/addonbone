cd1bc96894e96c92a97772bb54e80003787b2b70
Rostyslav Nihrutsa <rostyslav.nihrutsa@gmail.com>
2025-04-04 14:48:50 +0300

    Renamed some api files MBA-77

--- src/entry/command/Builder.ts
+++ src/entry/command/Builder.ts
@@ -1,5 +1,5 @@
 import {onActionClicked} from "@browser/action";
-import {onCommand} from "@browser/commands";
+import {onCommand} from "@browser/command";
 
 import {isValidCommandExecuteFunction, isValidCommandName} from "./resolvers";
 

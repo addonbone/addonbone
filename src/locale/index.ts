1c37350e36cc175f3b61e9123e73bac2d48f11f7
Addon Bone <jacobneemila@gmail.com>
2025-04-09 00:29:12 +0300

    Enhance locale creation: Introduce createLocale function to instantiate LocaleNative and streamline locale provider exports

--- src/locale/index.ts
+++ src/locale/index.ts
@@ -1,5 +1,9 @@
+import {LocaleProvider} from "@typing/locale";
+import LocaleNative, {type LocaleNativeStructure} from "./LocaleNative";
+
+export const createLocale = (): LocaleProvider<LocaleNativeStructure> => new LocaleNative;
+
+export {default as LocaleAbstract} from "./LocaleAbstract";
+
 export {Language, LanguageCodes, LocaleDir} from "@typing/locale";
-
-export type {LocaleNativeStructure} from "./LocaleNative";
-
-export {default as LocaleNative} from "./LocaleNative";
+export type {LocaleNativeStructure};

108e86939cc7e5d60ed94a2bcef443718a99c4ea
Rostyslav Nihrutsa <rostyslav.nihrutsa@gmail.com>
2025-04-10 18:06:56 +0300

    Fixed the types for useStorage

--- src/storage/useStorage.ts
+++ src/storage/useStorage.ts
@@ -9,15 +9,24 @@ interface UseStorageOptions<T> {
     defaultValue?: T;
 }
 
-type UseStorageReturnValue<T> = [T | undefined, (value: T) => void, () => void];
+type UseStorageReturnValue<T> = readonly [T | undefined, (value: T) => void, () => void];
+
+function isOptions<T>(arg: any): arg is UseStorageOptions<T> {
+    if (typeof arg === 'object' && arg !== null && 'key' in arg) {
+        if (typeof arg.key !== "string") {
+            throw new Error('Key must be a string');
+        }
+        return true;
+    }
+    return false;
+}
 
 function useStorage<T = any>(options: UseStorageOptions<T>): UseStorageReturnValue<T>;
 function useStorage<T = any>(key: string, defaultValue?: T): UseStorageReturnValue<T>
 function useStorage<T = any>(arg1: string | UseStorageOptions<T>, arg2?: T): UseStorageReturnValue<T> {
-    const isObject = typeof arg1 === "object";
-    const key = isObject ? arg1.key : arg1;
-    const storageRef = useRef(isObject ? arg1.storage ?? Storage.Local<Record<string, T>>() : Storage.Local<Record<string, T>>());
-    const defaultValue = useMemo(() => isObject ? arg1.defaultValue : arg2, [arg1, arg2]);
+    const key = isOptions(arg1) ? arg1?.key : arg1;
+    const storageRef = useRef(isOptions(arg1) ? arg1?.storage ?? Storage.Local<Record<string, any>>() : Storage.Local<Record<string, any>>());
+    const defaultValue = useMemo(() => isOptions(arg1) ? arg1?.defaultValue : arg2, [arg1, arg2]);
 
     const [value, setValue] = useState<T | undefined>(undefined);
 

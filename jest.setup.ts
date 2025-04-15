0e2571bd108832195e5bc57b6ad435af1055e894
Rostyslav Nihrutsa <rostyslav.nihrutsa@gmail.com>
2025-04-15 20:48:45 +0300

    Improved tests for Storage

--- jest.setup.ts
+++ jest.setup.ts
@@ -1,7 +1,8 @@
 import 'jest-webextension-mock';
+import BaseStorage from './src/storage/BaseStorage'
 
 const listeners: Array<
-    (changes: {[p: string]: chrome.storage.StorageChange}, areaName: chrome.storage.AreaName) => void
+    (changes: { [p: string]: chrome.storage.StorageChange }, areaName: chrome.storage.AreaName) => void
 > = [];
 
 chrome.storage.onChanged.addListener = (fn) => {
@@ -19,22 +20,33 @@ chrome.storage.onChanged.hasListener = (fn) => {
     return listeners.includes(fn);
 };
 
-global.simulateStorageChange = (
-    changes: {[p: string]: chrome.storage.StorageChange},
-    areaName: chrome.storage.AreaName = 'local'
-) => {
-    for (const listener of listeners) {
-        listener(changes, areaName);
-    }
-};
+interface StorageChange {
+    storage: BaseStorage<any>,
+    key: string,
+    oldValue: any,
+    newValue: any,
+    areaName: chrome.storage.AreaName
+}
 
-global.storageLocalGet = (key: string | string[]): Promise<any> => {
+global.simulateStorageChange = ({storage, key, oldValue, newValue, areaName = 'local'}: StorageChange) => {
+    const fullKey = storage['getFullKey'](key);
+
+    const changes = {[fullKey]: {oldValue, newValue}}
+
+    listeners.forEach((listener) => listener(changes, areaName))
+}
+
+// Needed to access a specific key in Storage
+// Native GET method does not work correctly with a specific key other than "key"
+// Pull Request with bug fix - https://github.com/RickyMarou/jest-webextension-mock/pull/19
+global.storageLocalGet = (key: string | string[], storage: BaseStorage<any>): Promise<any> => {
+    const formatKey = (key: string) => storage ? storage['getFullKey'](key) : key;
     return new Promise(resolve => {
         chrome.storage.local.get(null, (res) => {
             resolve(
                 Array.isArray(key)
-                    ? key.reduce((acc, k) => ({ ...acc, [k]: res[k] }), {})
-                    : res[key]
+                    ? key.reduce((acc, k) => ({...acc, [formatKey(k)]: res[formatKey(k)]}), {})
+                    : res[formatKey(key)]
             );
         });
     });
--- src/storage/Storage.test.ts
+++ src/storage/Storage.test.ts
@@ -1,9 +1,9 @@
 import {Storage} from "./Storage";
 
-describe('Storage: basic operations', () => {
+describe('Storage: set, get, getAll, clear, remove, watch methods', () => {
     const namespace = 'user'
     const storage = new Storage();
-    const secondStorage = new Storage({namespace});
+    const storageWithNamespace = new Storage({namespace});
 
     beforeEach(async () => {
         await chrome.storage.local.clear();
@@ -16,68 +16,68 @@ describe('Storage: basic operations', () => {
         ['null', null],
         ['object', {a: 1, b: true}],
         ['array', [1, 2, 3]],
-    ])('set/get %s', async (_, value) => {
+    ])('set/get - works with %s', async (_, value) => {
         await storage.set('key', value);
         const result = await storage.get('key');
         expect(result).toEqual(value);
     });
 
     test('set - saves data with namespace', async () => {
-        await secondStorage.set('theme', 'dark');
-        const result = await global.storageLocalGet(`${namespace}:theme`);
-        const all = await secondStorage.getAll();
+        await storageWithNamespace.set('theme', 'dark');
+        const result = await global.storageLocalGet('theme', storageWithNamespace);
+        const secondResult = (await storageWithNamespace.getAll())['theme'];
 
         expect(result).toEqual('dark');
-        expect(all['theme']).toEqual('dark');
+        expect(secondResult).toEqual('dark');
     });
 
     test('getAll - returns only saved values', async () => {
         await storage.set('a', 1);
         await storage.set('b', 2);
-        await secondStorage.set('c', 3);
-        await secondStorage.set('d', 4);
+        await storageWithNamespace.set('c', 3);
+        await storageWithNamespace.set('d', 4);
 
         const result = await storage.getAll();
-        const secondResult = await secondStorage.getAll();
+        const resultWithNamespace = await storageWithNamespace.getAll();
 
         expect(result).toEqual({a: 1, b: 2});
-        expect(secondResult).toEqual({c: 3, d: 4});
+        expect(resultWithNamespace).toEqual({c: 3, d: 4});
     });
 
     test('remove - deletes the key', async () => {
-        await storage.set('key', 1);
-        await storage.remove('key');
-        const result = await storage.get('key');
+        await storage.set('theme', 'dark');
+        await storage.remove('theme');
+        const result = (await storage.getAll())['theme'];
+        expect(result).toBeUndefined();
+    });
+
+    test('remove - deletes the key with namespace', async () => {
+        await storageWithNamespace.set('theme', 'dark');
+        await storageWithNamespace.remove('theme');
+        const result = (await storageWithNamespace.getAll())['theme'];
         expect(result).toBeUndefined();
     });
 
     test('clear - removes all keys from current namespace', async () => {
         await storage.set('a', 1);
         await storage.set('b', 2);
-        await secondStorage.set('c', 3);
-        await secondStorage.set('d', 4);
+        await storageWithNamespace.set('c', 3);
+        await storageWithNamespace.set('d', 4);
 
         await storage.clear();
 
         const result = await storage.getAll();
-        const secondResult = await secondStorage.getAll();
+        const resultWithNamespace = await storageWithNamespace.getAll();
 
         expect(result).toEqual({});
-        expect(secondResult).toEqual({c: 3, d: 4});
+        expect(resultWithNamespace).toEqual({c: 3, d: 4});
     });
 
     test('watch - calls specific key callback on change', () => {
         const keyCallback = jest.fn();
         storage.watch({theme: keyCallback});
 
-        global.simulateStorageChange(
-            {
-                theme: {
-                    oldValue: 'light',
-                    newValue: 'dark',
-                }
-            }
-        );
+        global.simulateStorageChange({storage, key: 'theme', oldValue: 'light', newValue: 'dark'});
 
         expect(keyCallback).toHaveBeenCalledWith('dark', 'light');
     });
@@ -86,14 +86,7 @@ describe('Storage: basic operations', () => {
         const keyCallback = jest.fn();
         storage.watch({theme: keyCallback});
 
-        global.simulateStorageChange(
-            {
-                volume: {
-                    oldValue: 50,
-                    newValue: 80,
-                },
-            }
-        );
+        global.simulateStorageChange({storage, key: 'volume', oldValue: 50, newValue: 80});
 
         expect(keyCallback).not.toHaveBeenCalled();
     });
@@ -102,18 +95,8 @@ describe('Storage: basic operations', () => {
         const globalCallback = jest.fn();
         storage.watch(globalCallback);
 
-        global.simulateStorageChange(
-            {
-                volume: {
-                    oldValue: 50,
-                    newValue: 80,
-                },
-                theme: {
-                    oldValue: 'light',
-                    newValue: 'dark',
-                }
-            }
-        );
+        global.simulateStorageChange({storage, key: 'theme', oldValue: 'light', newValue: 'dark'});
+        global.simulateStorageChange({storage, key: 'volume', oldValue: 50, newValue: 80});
 
         expect(globalCallback).toHaveBeenCalledWith(80, 50);
         expect(globalCallback).toHaveBeenCalledWith('dark', 'light');
@@ -125,18 +108,8 @@ describe('Storage: basic operations', () => {
         storage.watch({theme: keyCallback});
         storage.watch(globalCallback);
 
-        global.simulateStorageChange(
-            {
-                theme: {
-                    oldValue: 'light',
-                    newValue: 'dark',
-                },
-                volume: {
-                    oldValue: 50,
-                    newValue: 80,
-                }
-            }
-        );
+        global.simulateStorageChange({storage, key: 'theme', oldValue: 'light', newValue: 'dark'});
+        global.simulateStorageChange({storage, key: 'volume', oldValue: 50, newValue: 80});
 
         expect(keyCallback).toHaveBeenCalledWith('dark', 'light');
         expect(globalCallback).toHaveBeenCalledWith(80, 50);

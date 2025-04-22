c7471423c5d48cd09bd4cab1f2e9ab675ad04aa8
Rostyslav Nihrutsa <rostyslav.nihrutsa@gmail.com>
2025-04-23 00:29:33 +0300

    Updated the Message class tests

--- src/message/Message.test.ts
+++ src/message/Message.test.ts
@@ -3,25 +3,35 @@ import {MessageSender} from "../types/message";
 
 jest.mock('nanoid', () => ({nanoid: () => 'mocked-id'}));
 
-let listeners: any[] = [];
-chrome.runtime.onMessage.addListener = jest.fn((cb) => listeners.push(cb));
-chrome.runtime.onMessage.removeListener = jest.fn((cb) => {
-    const index = listeners.indexOf(cb);
-    if (index >= 0) listeners.splice(index, 1);
-});
+let listener: ((...args: any[]) => boolean | void) | null = null;
+
+chrome.runtime.onMessage.addListener = jest.fn((cb) => listener = cb);
+chrome.runtime.onMessage.removeListener = jest.fn((cb) => listener === cb && (listener = null));
 
 (chrome.runtime.sendMessage as jest.Mock).mockImplementation((msg, callback) => {
-    for (const listener of listeners) {
-        const handled = listener(msg, {} as MessageSender, (res: any) => callback?.(res));
-        if (handled) break;
-    }
+    if (!listener) return;
+
+    let called = false;
+    const result = listener(msg, {} as MessageSender, (response: any) => {
+        callback?.(response);
+        called = true;
+    });
+
+    if (result === true) return;
+    if (!called) callback?.(undefined);
 });
 
 (chrome.tabs.sendMessage as jest.Mock).mockImplementation((tabId, msg, options, callback) => {
-    for (const listener of listeners) {
-        const handled = listener(msg, {} as MessageSender, (res: any) => callback?.(res));
-        if (handled) break;
-    }
+    if (!listener) return;
+
+    let called = false;
+    const result = listener(msg, {} as MessageSender, (response: any) => {
+        callback?.(response);
+        called = true;
+    });
+
+    if (result === true) return;
+    if (!called) callback?.(undefined);
 });
 
 type MessageMap = {
@@ -31,31 +41,30 @@ type MessageMap = {
     fetchUser: (name: string) => Promise<{ name: string }>;
 };
 
+let message: Message<MessageMap>;
+
 beforeEach(async () => {
-    listeners = [];
+    listener = null
     jest.clearAllMocks();
+    message = new Message<MessageMap>();
 });
 
 describe('watch method', () => {
     test('adds and removes the message listener on subscribe and unsubscribe', async () => {
-        const message = new Message<MessageMap>();
-
-        expect(listeners.length).toBe(0);
+        expect(listener).toBe(null);
 
         const unsubscribe = message.watch('getStringLength', (str: string) => str.length);
 
-        expect(listeners.length).toBe(1);
+        expect(listener).not.toBe(null);
         expect(chrome.runtime.onMessage.addListener).toHaveBeenCalledWith(expect.any(Function));
 
         unsubscribe()
 
-        expect(listeners.length).toBe(0);
+        expect(listener).toBe(null);
         expect(chrome.runtime.onMessage.removeListener).toHaveBeenCalled();
     });
 
     test('registers a specific handler for a given message type', async () => {
-        const message = new Message<MessageMap>();
-
         message.watch('getStringLength', (str: string) => str.length);
         message.watch('toUpperCase', (str: string) => str.toUpperCase());
 
@@ -64,11 +73,10 @@ describe('watch method', () => {
 
         expect(result_1).toBe(4);
         expect(result_2).toBe('TEST');
-        expect(chrome.runtime.onMessage.addListener).toHaveBeenCalledTimes(2);
+        expect(chrome.runtime.onMessage.addListener).toHaveBeenCalledTimes(1);
     });
 
     test('registers async handler and resolves with its returned value', async () => {
-        const message = new Message<MessageMap>();
         message.watch('fetchUser', (name) => new Promise(resolve => setTimeout(() => resolve({name}), 100)));
 
         const result = await message.send('fetchUser', 'Tom');
@@ -77,7 +85,6 @@ describe('watch method', () => {
     });
 
     test('registers multiple handlers using a handler object', async () => {
-        const message = new Message<MessageMap>();
         message.watch({
             toUpperCase: (str) => str.toUpperCase(),
             getStringLength: (str) => str.length,
@@ -88,12 +95,10 @@ describe('watch method', () => {
 
         expect(result_1).toBe(4);
         expect(result_2).toBe('TEST');
-        expect(chrome.runtime.onMessage.addListener).toHaveBeenCalledWith(expect.any(Function));
         expect(chrome.runtime.onMessage.addListener).toHaveBeenCalledTimes(1);
     });
 
     test('registers a general handler for all message types', async () => {
-        const message = new Message<MessageMap>();
         message.watch((type, data) => console.log(`TYPE: ${type}, DATA: ${data}`));
 
         const result_1 = await message.send('getStringLength', 'test')
@@ -101,13 +106,10 @@ describe('watch method', () => {
 
         expect(result_1).toBe(undefined);
         expect(result_2).toBe(undefined);
-        expect(chrome.runtime.onMessage.addListener).toHaveBeenCalledWith(expect.any(Function));
         expect(chrome.runtime.onMessage.addListener).toHaveBeenCalledTimes(1);
     });
 
     test('prioritizes the first registered handler over others for the same message type', async () => {
-        const message = new Message<MessageMap>();
-
         message.watch('getStringLength', (str: string) => str.length);
         message.watch((type, data) => console.log(`TYPE: ${type}, DATA: ${data}`));
 
@@ -116,14 +118,12 @@ describe('watch method', () => {
 
         expect(result_1).toBe(4);
         expect(result_2).toBe(undefined);
-        expect(chrome.runtime.onMessage.addListener).toHaveBeenCalledWith(expect.any(Function));
-        expect(chrome.runtime.onMessage.addListener).toHaveBeenCalledTimes(2);
+        expect(chrome.runtime.onMessage.addListener).toHaveBeenCalledTimes(1);
     });
 })
 
 describe('send method', () => {
     test('sends a message and returns the correct response from the handler', async () => {
-        const message = new Message<MessageMap>();
         message.watch('getStringLength', (str) => str.length);
 
         const result = await message.send('getStringLength', 'test');
@@ -132,19 +132,14 @@ describe('send method', () => {
     });
 
     test('sends a message without data and receive a correct response from the handler', async () => {
-        const message = new Message<MessageMap>();
-
         message.watch('sayHello', () => 'Hello');
 
         const result = await message.send('sayHello', undefined);
 
         expect(result).toBe("Hello");
-        expect(chrome.runtime.onMessage.addListener).toHaveBeenCalledWith(expect.any(Function));
     });
 
     test('sends a message with correct structure', async () => {
-        const message = new Message<MessageMap>();
-
         message.watch('getStringLength', (str) => str.length);
 
         const result = await message.send('getStringLength', 'test');
@@ -162,7 +157,6 @@ describe('send method', () => {
     });
 
     test('sends a message to tab when options is a number', async () => {
-        const message = new Message<MessageMap>();
         message.watch('getStringLength', (str) => str.length);
 
         const result = await message.send('getStringLength', 'test', 123);
@@ -177,7 +171,6 @@ describe('send method', () => {
     });
 
     test('sends a message to tab when options is a object with tabId and frameId', async () => {
-        const message = new Message<MessageMap>();
         message.watch('getStringLength', (str) => str.length);
 
         const result = await message.send('getStringLength', 'test', {tabId: 123, frameId: 1});
@@ -191,3 +184,60 @@ describe('send method', () => {
         expect(result).toBe(4);
     });
 })
+
+describe('multiple handlers error for same message type', () => {
+    const errorMessage = 'Message type "getStringLength" has multiple handlers returning a response. Only one response is allowed.'
+
+    test('with two "type" handlers', async () => {
+        message.watch('getStringLength', () => 1);
+        message.watch('getStringLength', () => 2);
+
+        await expect(message.send('getStringLength', 'test')).rejects.toThrow(errorMessage);
+    });
+
+    test('with two "map" handlers', async () => {
+        message.watch({getStringLength: () => 1});
+        message.watch({getStringLength: () => 2});
+
+        await expect(message.send('getStringLength', 'test')).rejects.toThrow(errorMessage);
+    });
+
+    test('with two "general" handlers', async () => {
+        message.watch(() => 1);
+        message.watch(() => 2);
+
+        await expect(message.send('getStringLength', 'test')).rejects.toThrow(errorMessage);
+    });
+
+    test('with "type" and "map" handlers', async () => {
+        message.watch('getStringLength', () => 1);
+        message.watch({getStringLength: () => 1});
+
+        await expect(message.send('getStringLength', 'test')).rejects.toThrow(errorMessage);
+    });
+
+    test('with "type" and "general" handlers', async () => {
+        message.watch('getStringLength', () => 1);
+        message.watch(() => 2);
+
+        await expect(message.send('getStringLength', 'test')).rejects.toThrow(errorMessage);
+    });
+
+    test('with "map" and "general" handlers', async () => {
+        message.watch({getStringLength: () => 1});
+        message.watch(() => 2);
+
+        await expect(message.send('getStringLength', 'test')).rejects.toThrow(errorMessage);
+    });
+
+    test('allows multiple handlers if one of them don\'t return value', async () => {
+        message.watch('getStringLength', (data) => data.length);
+        message.watch((type, data) => {
+            if (type === 'toUpperCase') {
+                return data?.toUpperCase()
+            }
+        });
+
+        expect(await message.send('getStringLength', 'test')).toBe(4)
+    });
+});

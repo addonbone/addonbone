f62d755d02833d0754e7ea4b99d049f30c561756
Rostyslav Nihrutsa <rostyslav.nihrutsa@gmail.com>
2025-05-02 15:27:18 +0300

    Added using RelayType type instead object in function helpers

--- src/relay/index.ts
+++ src/relay/index.ts
@@ -1,14 +1,16 @@
 import ProxyRelay from "./ProxyRelay";
 import BaseRelay from "./BaseRelay";
 
+import {RelayType} from "@typing/relay";
+
 export {default as ProxyRelay} from './ProxyRelay'
 export {default as RegisterRelay} from "./RegisterRelay";
 
-export const getRelay = <T extends object>(name: string, tabId: number, frameId?: number) => {
+export const getRelay = <T extends RelayType>(name: string, tabId: number, frameId?: number) => {
     return new ProxyRelay<T>(name).get(tabId, frameId);
 }
 
-export const getRegisteredRelay = <T extends object>(name: string) => {
+export const getRegisteredRelay = <T extends RelayType>(name: string) => {
     return new BaseRelay<T>(name).get();
 }
 
